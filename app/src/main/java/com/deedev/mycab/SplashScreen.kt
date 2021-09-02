60d157760adde938b1b4d931423b5751ef233809
Deepak5310 <deepakjangir5310@gmail.com>
2021-09-02 23:33:10 +0530

    splash screen for real loading time + 2 seconds

--- app/src/main/java/com/deedev/mycab/SplashScreen.kt
+++ app/src/main/java/com/deedev/mycab/SplashScreen.kt
@@ -1,14 +1,28 @@
 package com.deedev.mycab
 
+import android.annotation.SuppressLint
 import android.content.Intent
-import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import androidx.appcompat.app.AppCompatActivity
 
+@SuppressLint("CustomSplashScreen")
 class SplashScreen : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
 
-        startActivity(Intent(applicationContext, MainActivity::class.java))
-        finish()
+    private val runnable = Runnable {
+        if (!isFinishing) {
+            startActivity(Intent(applicationContext, MainActivity::class.java))
+            finish()
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Handler(Looper.getMainLooper()).postDelayed(runnable, 2000) // This will display splash screen for real loading time + 2 seconds
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Handler(Looper.getMainLooper()).removeCallbacks(runnable)
     }
 }
